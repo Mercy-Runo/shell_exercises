7a7b625960c95b4f0ce4acfb0164d476dfc2fe29
Mercy Runo <mercyruno37@gmail.com>
2022-08-29 05:05:41 -0700

    print_arguments.c added

---
+++ print_arguments.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments without using ac
+ * @ac: number of itmes in av
+ * @av: NULL terminated array of strings
+ * Return: count
+ */
+
+int main(int ac, char **av)
+{
+  int i, count;
+
+  for (i = 0; av[i] != NULL; i++)
+    count = i + 1;
+
+  printf("%d", count);
+      
+  return (count);
+}
