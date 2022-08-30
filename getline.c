a5bc51f0bfece903115f02e81f845daa67951823
Mercy Runo <mercyruno37@gmail.com>
2022-08-30 07:23:52 -0700

    getline.c added

---
+++ getline.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - reads string input from user, then prints it using getline()
+ * Return: 0
+ */
+
+//int input(char *s, int length);
+
+int main()
+{
+  char *buffer;
+  size_t bufsize = 45;
+  size_t characters;
+
+  buffer = (char *)malloc(bufsize * sizeof(char));
+
+  if (buffer == NULL)
+  {
+    perror("unable to locate buffer");
+    exit (1);
+  }
+  else
+  {
+    printf("Type something: ");
+    characters = getline(&buffer, &bufsize, stdin);
+    printf("%zu characters read.\nYou typed: %s \n", characters, buffer);
+  }
+  return (0);
+}
